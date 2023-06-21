08b1b1af5079c8749de2c800df83980cc53e9f2b
yuvar <yuvar@DESKTOP-TM7NQ9Q>
2023-06-21 11:58:32 +0530

    Initial commit

---
+++ JenkinsPractise/src/com/practise/JenkinsPrac.java
@@ -0,0 +1,14 @@
+package com.practise;
+
+public class JenkinsPrac {
+	public static void main(String[] args) {
+		 System.out.println("car");
+		 System.out.println("bike");
+
+		 System.out.println("truck");
+
+		 System.out.println("cycle");
+
+	}
+
+}
